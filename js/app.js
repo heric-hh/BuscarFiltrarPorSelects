58d7f64e8d27d74b92853d2891057772aa1d516f
Eric Hernández <erhh.015@gmail.com>
2023-12-18 00:40:38 -0600

    Filtrar y buscar en base a diferentes campos

---
+++ js/app.js
@@ -0,0 +1,261 @@
+const marca = document.querySelector( "#marca" );
+const year = document.querySelector( "#year" );
+const minimo = document.querySelector( "#minimo" );
+const maximo = document.querySelector( "#maximo" );
+const puertas = document.querySelector( "#puertas" );
+const transmision = document.querySelector( "#transmision" );
+const color = document.querySelector( "#color" );
+
+const resultado = document.querySelector( "#resultado" );
+const yearMax = new Date().getFullYear();
+const yearMin = yearMax - 10;
+
+const datosBusqueda = {
+    marca: "",
+    year: "",
+    minimo: "",
+    maximo: "",
+    puertas: "",
+    transmision: "",
+    color: ""
+};
+
+document.addEventListener( "DOMContentLoaded" , () => {
+
+    mostrarAutos( autos );
+    llenarSelect();
+
+});
+
+
+
+//* Event listener para los select de busqueda
+
+marca.addEventListener( "change" , e => {
+    
+    datosBusqueda.marca = e.target.value;
+    filtrarAuto();
+
+});
+
+
+
+year.addEventListener( "change" , e => {
+    
+    datosBusqueda.year = parseInt ( e.target.value );
+    filtrarAuto();
+
+});
+
+
+
+minimo.addEventListener( "change" , e => {
+    
+    datosBusqueda.minimo = e.target.value;
+    filtrarAuto();
+
+});
+
+
+
+maximo.addEventListener( "change" , e => {
+    
+    datosBusqueda.maximo = e.target.value;
+    filtrarAuto();
+
+});
+
+
+puertas.addEventListener( "change" , e => {
+    
+    datosBusqueda.puertas = parseInt( e.target.value );
+    filtrarAuto();
+
+});
+
+
+
+transmision.addEventListener( "change" , e => {
+    
+    datosBusqueda.transmision = e.target.value;
+    filtrarAuto();
+
+});
+
+
+
+color.addEventListener( "change" , e => {
+    
+    datosBusqueda.color = e.target.value;
+    filtrarAuto();
+    console.log(datosBusqueda)
+});
+
+
+
+function mostrarAutos( autos ) {
+
+    limpiarHTML(); //Elimina el HTML previo
+
+    autos.forEach( auto => {
+
+        const { marca, modelo , year , puertas , transmision , precio , color } = auto;
+        const autoHTML = document.createElement( "P" );
+        autoHTML.textContent = `
+            ${marca} ${modelo} - ${year} - ${puertas} Puertas - Transmision ${transmision} - Precio: ${precio}
+            - Color: ${color}
+
+        `;
+
+        resultado.appendChild( autoHTML );
+    });
+}
+
+
+
+function limpiarHTML() {
+
+    while( resultado.firstChild )
+        resultado.removeChild( resultado.firstChild );
+
+}
+
+
+
+function llenarSelect() {
+
+    for( let i = yearMax; i >= yearMin; i-- ) {
+        
+        const opcion = document.createElement( "OPTION" );
+        opcion.value = i;
+        opcion.textContent = i;
+        year.appendChild( opcion );
+
+    }
+}
+
+
+
+function filtrarAuto() {
+
+    const resultado = 
+        autos.filter( filtrarMarca )
+        .filter( filtrarYear )
+        .filter( filtrarMinimo )
+        .filter( filtrarMaximo )
+        .filter( filtrarPuertas )
+        .filter( filtrarTransmision )
+        .filter( filtrarColor );
+    
+    console.log( resultado );
+
+    if ( resultado.length ) 
+        mostrarAutos( resultado );
+    else
+        noResultado();
+
+}
+
+
+
+function filtrarMarca( auto ) {
+
+    const { marca } = datosBusqueda;
+    
+    if ( marca )
+        return auto.marca === marca;
+
+    return auto;
+    
+}
+
+
+
+function filtrarYear( auto ) {
+
+    const { year } = datosBusqueda;
+    
+    
+    
+    if ( year )
+        return auto.year === year ;
+
+    return auto;
+
+}
+
+
+
+function filtrarMinimo( auto ) {
+
+    const { minimo } = datosBusqueda;
+    
+    if ( minimo )
+        return auto.precio >= minimo ;
+
+    return auto;
+
+}
+
+
+
+function filtrarMaximo( auto ) {
+
+    const { maximo } = datosBusqueda;
+    
+    if ( maximo )
+        return auto.precio <= maximo ;
+
+    return auto;
+
+}
+
+
+
+function filtrarPuertas( auto ) {
+
+    const { puertas } = datosBusqueda;
+    
+    if ( puertas )
+        return auto.puertas === puertas ;
+
+    return auto;
+
+}
+
+
+
+function filtrarTransmision( auto ) {
+
+    const { transmision } = datosBusqueda;
+    
+    if ( transmision )
+        return auto.transmision === transmision ;
+
+    return auto;
+
+}
+
+
+
+function filtrarColor( auto ) {
+
+    const { color } = datosBusqueda;
+    
+    if ( color )
+        return auto.color === color ;
+
+    return auto;
+}
+
+
+
+function noResultado() {
+
+    limpiarHTML();
+    const noResultado = document.createElement( "DIV" );
+    noResultado.classList.add( "alerta" , "error" );
+    noResultado.textContent = "No Hay Resultados";
+    resultado.appendChild( noResultado );
+
+}
